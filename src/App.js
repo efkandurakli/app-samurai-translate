ca5d8ab6fcbda6879db94ede9e09cbd4c3b9e0e3
efkan.durakli <efkan.durakli@accenture.com>
2021-11-13 11:28:05 +0300

    implement body component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '@components/Header';
+import Body from 'components/Body';
 import '@styles/base.scss';
 
 function App() {
   return <div className="App">
-    <Header></Header>
+    <Header />
+    <Body />
   </div>;
 }
 
---
+++ src/components/Body/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Body = () => {
+
+    return <div className='h-100-px b-b-1 bc-alto bg-alabaster' />
+}
+
+export default Body;
