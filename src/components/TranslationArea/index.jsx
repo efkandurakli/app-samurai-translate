547dcbbbd887a067a3603160f041afc528e5d827
efkan.durakli <efkan.durakli@accenture.com>
2021-11-14 13:30:38 +0300

    wrap translation are with container

--- src/components/TranslationArea/index.jsx
+++ src/components/TranslationArea/index.jsx
@@ -72,80 +72,82 @@ const TranslationArea = () => {
   }, [data, activeSourceLanguageId, activeTargetLanguageId]);
 
   return (
-    <div className="translation-area">
-      <div className="tabs b-b-1 bc-alto">
-        <div className="tabs__source-languages">
-          {LANGUAGES.map((language, index) => {
-            const isActive = activeSourceLanguageId === language.id;
-            return (
-              <p
-                className={`language-title p-l-4 p-r-4 p-t-2 cursor-pointer ${
-                  isActive ? "b-b-2 bc-fun-blue color-fun-blue" : ""
-                }`}
-                key={`${language.id}-${index}`}
-                onClick={() => handleSourceLanguageClick(language.id)}
-              >
-                {language.text}
-              </p>
-            );
-          })}
-        </div>
-
-        <div className="tabs__target-languages">
-          <img src={swapIcon} height="40px" width="40px" />
-          <div className="disp-flex p-l-5">
-            {LANGUAGES.reverse().map((language, index) => {
-              const isActive = activeTargetLanguageId === language.id;
+    <div className='container'>
+      <div className="translation-area">
+        <div className="tabs b-b-1 bc-alto">
+          <div className="tabs__source-languages">
+            {LANGUAGES.map((language, index) => {
+              const isActive = activeSourceLanguageId === language.id;
               return (
                 <p
                   className={`language-title p-l-4 p-r-4 p-t-2 cursor-pointer ${
                     isActive ? "b-b-2 bc-fun-blue color-fun-blue" : ""
                   }`}
                   key={`${language.id}-${index}`}
-                  onClick={() => handleTargetLanguageClick(language.id)}
+                  onClick={() => handleSourceLanguageClick(language.id)}
                 >
                   {language.text}
                 </p>
               );
             })}
           </div>
-        </div>
-      </div>
-      <div className="disp-flex" style={{ minHeight: "225px" }}>
-        <div
-          className="w-50-p b-r-1 bc-alto disp-flex"
-          style={{ alignItems: "baseline" }}
-        >
-          <TextareaAutosize
-            className="text-area w-100-p"
-            value={sourceText}
-            rows={5}
-            onChange={(e) => handleSourceTextChange(e.target.value)}
-          />
-          {sourceText && (
-            <div
-              className="clear-icon p-r-2 cursor-pointer"
-              onClick={() => {
-                setSourceText("");
-                setTargetText("");
-              }}
-            >
-              <img src={clearIcon} />
+
+          <div className="tabs__target-languages">
+            <img src={swapIcon} height="40px" width="40px" />
+            <div className="disp-flex p-l-5">
+              {LANGUAGES.reverse().map((language, index) => {
+                const isActive = activeTargetLanguageId === language.id;
+                return (
+                  <p
+                    className={`language-title p-l-4 p-r-4 p-t-2 cursor-pointer ${
+                      isActive ? "b-b-2 bc-fun-blue color-fun-blue" : ""
+                    }`}
+                    key={`${language.id}-${index}`}
+                    onClick={() => handleTargetLanguageClick(language.id)}
+                  >
+                    {language.text}
+                  </p>
+                );
+              })}
             </div>
-          )}
+          </div>
         </div>
-        <div className="w-50-p">
-          <TextareaAutosize
-            className="text-area w-100-p"
-            value={targetText}
-            rows={5}
-            readOnly
-            onChange={(e) => setSourceText(e.target.value)}
-          />
+        <div className="disp-flex" style={{ minHeight: "225px" }}>
+          <div
+            className="w-50-p b-r-1 bc-alto disp-flex"
+            style={{ alignItems: "baseline" }}
+          >
+            <TextareaAutosize
+              className="text-area w-100-p"
+              value={sourceText}
+              rows={5}
+              onChange={(e) => handleSourceTextChange(e.target.value)}
+            />
+            {sourceText && (
+              <div
+                className="clear-icon p-r-2 cursor-pointer"
+                onClick={() => {
+                  setSourceText("");
+                  setTargetText("");
+                }}
+              >
+                <img src={clearIcon} />
+              </div>
+            )}
+          </div>
+          <div className="w-50-p">
+            <TextareaAutosize
+              className="text-area w-100-p"
+              value={targetText}
+              rows={5}
+              readOnly
+              onChange={(e) => setSourceText(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
-      <div className="p-2">
-        <img src={microphoneIcon} />
+        <div className="p-2">
+          <img src={microphoneIcon} />
+        </div>
       </div>
     </div>
   );
