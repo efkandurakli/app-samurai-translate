9ab01a087dd52e8619ca7565447dce41d8bd447f
efkan.durakli <efkan.durakli@accenture.com>
2021-11-22 10:34:07 +0300

    add console.log

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,9 @@
 import { setConfig } from "react-google-translate";
 
+console.log("REACT_APP_GCP_CLIENT_EMAIL : ", process.env.REACT_APP_GCP_CLIENT_EMAIL);
+console.log("REACT_APP_GCP_PRIVATE_KEY : ", process.env.REACT_APP_GCP_PRIVATE_KEY);
+console.log("REACT_APP_GCP_PROJECT_ID : ", process.env.REACT_APP_GCP_PROJECT_ID);
+
 export const setGoogleTranslateConfig = () => {
   setConfig({
     clientEmail: process.env.REACT_APP_GCP_CLIENT_EMAIL,
